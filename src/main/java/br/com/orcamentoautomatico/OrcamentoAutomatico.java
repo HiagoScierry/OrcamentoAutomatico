6d72312f5f197c48ced735af823b4e5a89069e3a
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2025-01-25 11:30:38 -0300

    projeto iniciado

---
+++ src/main/java/br/com/orcamentoautomatico/OrcamentoAutomatico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.orcamentoautomatico;
+
+/**
+ *
+ * @author hiago
+ */
+public class OrcamentoAutomatico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
